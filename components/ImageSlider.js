baa88efbbb38abf2bab33d77bc32ab058d254b00
Sebastian Andreasson <sebastian.andreasson93@gmail.com>
2021-04-27 22:38:50 +0200

    encodeURIComponent instead

--- components/ImageSlider.js
+++ components/ImageSlider.js
@@ -46,7 +46,7 @@ const ImageSlider = () => {
     for (let i = from; i < to; i++) {
       if (!imageCache[i]) {
         const preloadImage = new window.Image()
-        preloadImage.src = `/_next/image?url=${encodeURI(
+        preloadImage.src = `/_next/image?url=${encodeURIComponent(
           images[i].url
         )}&w=828&q=75`
         imageCache[i] = preloadImage
