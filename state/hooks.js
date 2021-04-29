a761215116c68ffed36e4e290dd30e7910b2eb01
Sebastian Andreasson <sebastian.andreasson93@gmail.com>
2021-04-29 20:31:18 +0200

    fetch from api instead of graphql

--- state/hooks.js
+++ state/hooks.js
@@ -1,63 +1,33 @@
 import { useEffect, useCallback } from 'react'
 import { useRecoilState } from 'recoil'
-import { useMutation, useQuery } from 'urql'
 import { imagesAtom, gurkAtom } from './index'
-
-const ImageQuery = `
-{
-  getImages(type: "front", _size: 5000) {
-    data {
-      taken
-      url
-    }
-  }
-}
-`
 
 export const useImages = () => {
   const [images, setImages] = useRecoilState(imagesAtom)
-  const [result] = useQuery({
-    query: ImageQuery,
-  })
-  const { data } = result
 
   useEffect(() => {
-    if (!!data) {
-      setImages(data.getImages.data)
+    const getData = async () => {
+      const res = await fetch('/api/images?type=front')
+      const data = await res.json()
+      setImages(data)
     }
-  }, [data, setImages])
+    getData()
+  }, [setImages])
 
   return images
 }
 
-const GurkQuery = `
-{
-  allGurkor {
-    data {
-      name
-      species
-      hobby
-      bio
-      personality
-      position
-      votes
-    }
-  }
-}
-`
-
 export const useGurkor = () => {
   const [gurkor, setGurkor] = useRecoilState(gurkAtom)
-  const [result] = useQuery({
-    query: GurkQuery,
-  })
-  const { data } = result
 
   useEffect(() => {
-    if (!!data) {
-      setGurkor(data.allGurkor.data)
+    const getData = async () => {
+      const res = await fetch('/api/gurkor')
+      const data = await res.json()
+      setGurkor(data)
     }
-  }, [data, setGurkor])
+    getData()
+  }, [setGurkor])
 
   return gurkor
 }
